9403d8b4d63b9b7eb7ec38a6d1c0d915880d97b6
ricardolino <ricardo.linorj@gmail.com>
2021-03-15 22:55:10 +0000

    fix(env): get env BOT_TOKEN

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const Discord = require("discord.js");
+require('dotenv/config');
 const { BOT_TOKEN } = process.env;
 
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.login(BOT_TOKEN);
